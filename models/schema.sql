dbb3e51f408d94fed660c9e9981083ba0d891c70
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-06-25 12:54:03 -0400

    beginning schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,4 +1,16 @@
 DROP DATABASE IF EXISTS dingdb;
 CREATE DATABASE dingdb;
 
-CREATE TABLE contributers;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id int NOT NULL AUTO_INCREMENT,
+    user_name varchar(32) NOT NULL,
+    belt_color NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS videos;
+CREATE TABLE videos;
+
