954f99e3386398330b68f7e006d5c66db5ba6bf0
joshuabyker <47902849+joshuabyker@users.noreply.github.com>
2019-06-29 08:25:44 -0700

    added more to js

--- Dojo-Main/js/search.js
+++ Dojo-Main/js/search.js
@@ -26,8 +26,8 @@ function gameSearch(){
     y.style.visibility = "hidden";
   }
 
-  
-}
+
+};
 // module.exports = function(sequelize, DataTypes) {
 //   var Example = sequelize.define("Example", {
 //     text: DataTypes.STRING,
@@ -37,3 +37,36 @@ function gameSearch(){
 // };
 
 // // Search.like
+var path = require ("path");
+var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname +"/../config/config.json")[env];
+var db = {};
+
+if(config.use_env_variable){
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+}else{
+  var sequelize= new Sequelize(config.database, config.genreSearch, config.gameSearch, config);
+
+}
+
+fs
+.readdirSync(__dirname)
+.filter(function(choices){
+  return(choices.indexOf(".") !==0)&&( choices !==basename) && (choices.slice(-3)===".js");
+})
+.forEach(function(choices){
+  var model = sequelize["import"](path.join(__dirname,choices));
+  db[model.name] = model;
+})
+
+Object.keys(db).forEach(function(ModelName){
+  if(db[ModelName].associate){
+    db[ModelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
