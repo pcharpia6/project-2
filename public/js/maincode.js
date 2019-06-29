b4e2abc50be1ce17bd621b59602d2ed839a91ffc
Katie Humburg <pookakat@gmail.com>
2019-06-29 12:20:17 -0400

    make iframe on main page to directly access outside pages

--- public/js/maincode.js
+++ public/js/maincode.js
@@ -2,6 +2,7 @@ $('body').fadeIn(1000);
 
 $(document).ready(function() {
     $('#title-text').animate({'margin-left': '0'}, 'slow');
+    $('#current-data').attr('src', '../ding-su-video-player/index.html');
 });
 
 $('#menu').click(function(){
