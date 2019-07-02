b3401db87262ebeaf1cf8d0454d2a91fe2461378
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-07-02 17:55:23 -0400

    get routes on apiRoutes, changing models to camelcase

--- models/coach_review.js
+++ models/coach_review.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var coachReview = sequelize.define("coachReview", {
+    Text: DataTypes.STRING,
+    Description: DataTypes.TEXT
   });
-  return Example;
+  return coachReview;
 };
